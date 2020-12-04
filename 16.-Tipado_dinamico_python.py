1739439e6695d0370b21881477911e9f8fc78a16
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 20:22:46 -0600

    Add files via upload

---
+++ 16.-Tipado_dinamico_python.py
@@ -0,0 +1,18 @@
+'''
+Este programa muestra el proceso de tipado dinámico, como cambia al cambiar el tipo de valor
+que se introduce.
+'''
+
+print("Este programa muestra el desarrollo del tipado dinámico en python")
+print("Este tipo de tipado o cambio de tipo dinámico de las variables se puede apreciar en Python y Ruby")
+variabledinamica = 3.1416
+print("\nEn un primer momento el valor de la variable es:",variabledinamica)
+print("En este momento la variable es del tipo: ")
+print(type(variabledinamica))
+variabledinamica = 7
+print("\nMás adelante en el programa la variable adquirió un nuevo valor: ",variabledinamica)
+print("Ahora la variable es del tipo: ")
+print(type(variabledinamica))
+print("\nEste programa fue escrito por Emilio Carcaño Bringas")
+
+# Este programa fue escrito por Emilio Carcaño Bringas
